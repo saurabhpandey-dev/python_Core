2736a753828d11babfbd23d1ec3a315dfca0c844
Saurabh Pandey <133998803+saurabhpandey-backend@users.noreply.github.com>
2026-06-10 23:22:05 +0530

    find() function
    working with find() function

---
+++ Working_with_string/str.find().py
@@ -0,0 +1,8 @@
+str.find(): This returns the index for the first occurrence of a substring. If one is not found, then -1 is returned:
+developer = 'Naomi'
+
+result = developer.find('N')
+print(result)  # 0
+
+city = 'Los Angeles'
+print(city.find('New')) # -1
