3eda747226c93c4541c33893705e764c028a6876
Saurabh Pandey <133998803+saurabhpandey-backend@users.noreply.github.com>
2026-06-10 23:24:40 +0530

    capitalize() function
    working with capitalize() function

---
+++ Working_with_string/str.capitalize().py
@@ -0,0 +1,5 @@
+##str.capitalize(): This returns a new string with the first character
+##capitalized and the other characters lowercased:
+
+dessert = 'chocolate cake'
+print(dessert.capitalize()) # Chocolate cake
