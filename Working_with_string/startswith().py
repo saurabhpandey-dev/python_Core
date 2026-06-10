83694c197b46c31befc332c27c5df524ac91fd3a
Saurabh Pandey <133998803+saurabhpandey-backend@users.noreply.github.com>
2026-06-10 23:23:00 +0530

    startwith() function
    working with startwith() function

---
+++ Working_with_string/startswith().py
@@ -0,0 +1,7 @@
+####str.startswith(prefix): This returns a boolean indicating if a string starts
+##with the specified prefix:
+
+developer = 'Naomi'
+
+result = developer.startswith('N')
+print(result)  # True
