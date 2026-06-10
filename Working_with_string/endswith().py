10b43cf5bba9e606d9ec3d6234e7ce7c80818e31
Saurabh Pandey <133998803+saurabhpandey-backend@users.noreply.github.com>
2026-06-10 23:23:31 +0530

    endwith() function
    working with endwith() function

---
+++ Working_with_string/endswith().py
@@ -0,0 +1,7 @@
+##str.endswith(suffix): This returns a boolean indicating if a string ends with
+##the specified suffix:
+
+developer = 'Naomi'
+
+result = developer.endswith('N')
+print(result)  # False
