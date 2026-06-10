401431368e60d16f64d53db0d3029e148ed4ae7c
Saurabh Pandey <133998803+saurabhpandey-backend@users.noreply.github.com>
2026-06-10 23:24:13 +0530

    join function/keyword
    working with join keyword/function

---
+++ Working_with_string/join_keyword.py
@@ -0,0 +1,9 @@
+##join(): This is used to join elements of an iterable into a string with a
+##separator. An iterable is a collection of elements that can be looped
+##over like a list, string or a tuple.
+
+
+example_list = ['example', 'dashed', 'name']
+
+joined_str = ' '.join(example_list)
+print(joined_str)  # example dashed name
