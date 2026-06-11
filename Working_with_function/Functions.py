8c9cb339115d8a24a14e8bbeb759b75bcca6529d
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-11 23:13:59 +0530

    own function
    creating own function

---
+++ Working_with_function/Functions.py
@@ -0,0 +1,11 @@
+##Functions are reusable pieces of code that take inputs (arguments) and
+##returns an output. To call a function, you need to reference the function
+##name followed by a set of parenthesis:
+# Defining a function
+
+
+def get_sum(num_1, num_2):
+    return num_1 + num_2
+
+result = get_sum(3, 4) # function call
+print(result) # 7
