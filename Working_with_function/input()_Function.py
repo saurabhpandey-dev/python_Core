5f16b63cb72676282df48d349575af082d9e1bdc
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-11 23:13:15 +0530

    input() function
    take user input from console using input() function

---
+++ Working_with_function/input()_Function.py
@@ -0,0 +1,4 @@
+##input() Function: This is used to prompt the user for some input:
+    
+name = input('What is your name?') # User types 'Kolade' and presses Enter  
+print('Hello', name) # Hello Saurabh
