8d321ca87d94efcefeeb88b37b2b8fcc67974c4e
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-12 22:50:52 +0530

    local scope
    add local scope understanding

---
+++ Scope_restriction/Local_Scope.py
@@ -0,0 +1,9 @@
+Local Scope: This is when a variable declared inside a function
+or class can only be accessed within that function or class.
+
+def my_func():
+    num = 10
+    print(num)
+
+my_func()
+##print(num)
