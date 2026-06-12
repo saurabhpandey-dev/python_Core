d6179dd8966fd5e68836cde953c006da214e7f88
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-12 22:53:43 +0530

    built-in-scope
    add built in scope understanding

---
+++ Scope_restriction/Built_in_Scope.py
@@ -0,0 +1,7 @@
+##Built-in Scope: Reserved names in Python for predefined functions, modules,
+##keywords, and objects.
+
+
+print(str(45)) # '45'
+print(type(3.14)) # <class 'float'>
+print(isinstance(3, str)) # False
