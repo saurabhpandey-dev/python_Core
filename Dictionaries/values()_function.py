78b50720dbdbf487628fb9016ddaa3d1962839d5
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-15 21:11:52 +0530

    value() function
    add and learn value() function

---
+++ Dictionaries/values()_function.py
@@ -0,0 +1,12 @@
+# values() return all the values ontaining all the values present in a dictionary
+
+pizza = {
+    'name': 'Margherita Pizza',
+    'price': 8.9,
+    'calories_per_slice': 250,
+    'toppings': ['mozzarella', 'basil']
+}
+
+
+print(pizza.values())
+#dict_values(['Margherita Pizza', 8.9, 250, ['mozzarella', 'basil']])
