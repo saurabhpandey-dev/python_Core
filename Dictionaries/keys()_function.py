2e4dcbb5f7d7ebcf5cf81a9b83bc07af8a36d6e2
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-15 21:11:25 +0530

    keys() function
    add and learn keys() function

---
+++ Dictionaries/keys()_function.py
@@ -0,0 +1,11 @@
+# key() return all key name who containing all the keys present in a dictionary.
+
+pizza = {
+         'name': 'Margherita Pizza',
+         'price': 8.9,
+         'calories_per_slice': 250,
+         'toppings': ['mozzarella', 'basil']
+         }
+
+print(pizza.keys())
+#dict_keys(['name', 'price', 'calories_per_slice', 'toppings'])
