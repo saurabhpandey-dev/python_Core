bcf167e561dfc9c9d7ef0072fc6b554fd8c454df
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-15 21:07:07 +0530

    create dictionary
    Add and learn  how to create dictionary

---
+++ Dictionaries/creates_dict.py
@@ -0,0 +1,8 @@
+pizza = {
+    'name': 'Margherita Pizza',
+    'price': 8.9,
+    'calories_per_slice': 250,
+    'toppings': ['mozzarella', 'basil']
+}
+print(pizza) # print all item's
+print(pizza['name'])  # print value acording to key 
