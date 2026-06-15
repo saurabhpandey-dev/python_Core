bebee780d7efe647a328abea2e2d105d58d64b75
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-15 21:07:44 +0530

    creation type
    creation type of dict

---
+++ Dictionaries/two_way_creationdict.py
@@ -0,0 +1,13 @@
+# here we create the dictionary in two way
+
+# way one
+pizza = {
+    'name': 'Margherita Pizza',
+    'price': 8.9,
+    'calories_per_slice': 250,
+    'toppings': ['mozzarella', 'basil']
+}
+
+
+#way two
+pizza = dict([('name', 'Margherita Pizza'), ('price', 8.9), ('calories_per_slice', 250), ('toppings', ['mozzarella', 'basil'])])
