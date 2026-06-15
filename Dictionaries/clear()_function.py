f7c7242200ea5e09e0a95ed0a8a33ba9b086365a
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-15 21:13:28 +0530

    clear() function
    add and learn clear() function

---
+++ Dictionaries/clear()_function.py
@@ -0,0 +1,17 @@
+##The .clear() method removes all the key-value pairs from the dictionary:
+
+pizza = {
+    'name': 'Margherita Pizza',
+    'price': 8.9,
+    'calories_per_slice': 250,
+    'toppings': ['mozzarella', 'basil']
+}
+
+print('having all data :-\n',pizza, '\n')
+
+pizza.clear()
+
+print('all data got deleted :- \n')
+print(pizza,'\n')
+
+
