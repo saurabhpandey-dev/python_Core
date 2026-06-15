6164ce09ef36c39f53ba6f3c687b33921ddb27e2
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-15 21:09:53 +0530

    change value
    add change value in dict

---
+++ Dictionaries/Change_value_using_key.py
@@ -0,0 +1,10 @@
+pizza = {
+    'name': 'Margherita Pizza',
+    'price': 8.9,
+    'calories_per_slice': 250,
+    'toppings': ['mozzarella', 'basil']
+}
+
+pizza['name'] = 'Double Cheese'
+
+print(pizza['name'])
