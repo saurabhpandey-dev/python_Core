042e1f0db986708194d357c9ed06e8ea7be37b2a
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-16 14:53:23 +0530

    iterate over the values()
    add iteration over the values

---
+++ Dictionaries/iteration_over_the_values.py
@@ -0,0 +1,15 @@
+products = {
+    'Laptop': 990,
+    'Smartphone': 600,
+    'Tablet': 250,
+    'Headphones': 70,
+}
+
+for product in products.values():
+    print(product)
+
+##products.values() to get all the values of the products dictionary, a colon,
+##and then the body of the loop, where you can apply any logic to the values.
+##In this case, we are printing them.
+##
+##The loop variable will take each one of the values, one per iteration.
