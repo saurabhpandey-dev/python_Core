019da777713635d81a16386aea4d18244a2250e8
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-26 23:38:43 +0530

    add __ne__() function

---
+++ Magic_methods_or_Dunder_methods/__ne__()function.py
@@ -0,0 +1,11 @@
+class Box:
+    def __init__(self, weight):
+        self.weight = weight
+
+    def __ne__(self, other):
+        return self.weight != other.weight
+
+box1 = Box(50) 
+box2 = Box(30)  
+
+print(box1 != box2) 
