658789d4a418b43eeaa5688999b352ac7aa7866f
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:31:19 +0530

    add index()_function
    Used to find the first index where an element can be found in a list.

---
+++ List/index()_function.py
@@ -0,0 +1,5 @@
+#index(): Used to find the first index where an element can be found in a list.
+
+
+programming_languages = ['Rust', 'Java', 'Python', 'C++']
+print(programming_languages.index('Java'))
