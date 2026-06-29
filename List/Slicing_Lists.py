8a2cd26175760900c9c041f366d3099a3724704d
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:34:46 +0530

    add Slicing Lists
    Slicing is the concept of accessing a portion of a list by
    #using the slice operator :.

---
+++ List/Slicing_Lists.py
@@ -0,0 +1,7 @@
+#Slicing Lists: Slicing is the concept of accessing a portion of a list by
+#using the slice operator :. To slice a list that starts at index 1 and ends
+#before index 3, you can use the following syntax.
+
+
+desserts = ['Cake', 'Cookies', 'Ice Cream', 'Pie']
+print(desserts[1:3])
