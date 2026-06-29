c37f4e98602e530f40adee642f248dccc5720821
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:30:37 +0530

    add Removing Elements
    Removing Elements from a List

---
+++ List/Removing_Elements_from_List.py
@@ -0,0 +1,8 @@
+#Removing Elements from a List: Elements can be removed from a list using the
+#del keyword.
+
+
+developer = ['Jane Doe', 23, 'Python Developer']
+print(developer)
+del developer[1]
+print(developer) 
