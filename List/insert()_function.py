e794bd4230939c9e2c4377753fe97fdaa3816e5d
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:32:13 +0530

    add insert() function
    Used to insert an item at a specific index in the list.

---
+++ List/insert()_function.py
@@ -0,0 +1,7 @@
+#insert(): Used to insert an item at a specific index in the list. Here is
+#an example of using the insert() method.
+
+numbers = [1, 2, 3, 4, 5]
+numbers.insert(2, 2.5)
+
+print(numbers) 
