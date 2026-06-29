03b1d971e8f4014de11e10db3dbb2889740bc42a
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:27:26 +0530

    add append()_function
    Used to add an item to the end of the list.

---
+++ List/Appending_lists.py
@@ -0,0 +1,8 @@
+#The append() method can also be used to add one list at the
+#end of another.
+
+numbers = [1, 2, 3, 4, 5]
+even_numbers = [6, 8, 10]
+
+numbers.append(even_numbers)
+print(numbers) 
---
+++ List/append()_function.py
@@ -0,0 +1,6 @@
+#append(): Used to add an item to the end of the list. Here is an example
+#of using the append() method to add the number 6 to this numbers list.
+
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers) 
