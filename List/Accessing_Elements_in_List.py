fe86fb11b3943de2c601be664b42636364f8bec7
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:24:14 +0530

    add Accessing Elements
    Accessing Elements in a List

---
+++ List/Accessing_Elements_in_List.py
@@ -0,0 +1,17 @@
+#Accessing Elements in a List: To access an element from the cities list,
+#you can reference its index number in the sequence.
+
+city = ['Mirzapur','Varansi','Prayagraj']
+
+print(city)
+print(city[1])
+print(city[2])
+
+#Accessing Elements Using Negative Indexing: To access the last element of
+#any list, you can use -1 as the index number.
+
+print(city[-1])
+print(city[-2])
+
+#Negative indexing is used to access elements starting from the end of the
+#list instead of the beginning at index 0.
