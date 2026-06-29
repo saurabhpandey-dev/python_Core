362ae146838a65c23b3ebd1052409a6cdf994a2c
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:33:55 +0530

    add remove() function
    Used to remove an item from the list.

---
+++ List/remove()_function.py
@@ -0,0 +1,7 @@
+#remove(): Used to remove an item from the list. The remove() method will
+#only remove the first occurrence of an item in the list.
+
+numbers = [1, 2, 3, 4, 5, 5, 5]
+numbers.remove(5)
+
+print(numbers) 
