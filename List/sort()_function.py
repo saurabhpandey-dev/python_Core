b6bd84c7c5c6155a3fa01b7e64d2084318f31575
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:32:42 +0530

    add sort() function
    The sort() method is used to sort the elements in place.

---
+++ List/sort()_function.py
@@ -0,0 +1,9 @@
+#sort(): The sort() method is used to sort the elements in place. Here is an
+#example of sorting a random list of numbers in place.
+
+numbers = [19, 2, 35, 1, 67, 41]
+print(numbers)
+
+numbers.sort()
+
+print(numbers) 
