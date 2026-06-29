2c4470d93278d245aaa4a0368a444c6773fc9cf0
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:27:46 +0530

    add  list() constructor
    Creating Lists Using the list() constructor

---
+++ List/Creating_Lists_Using_list().py
@@ -0,0 +1,9 @@
+#Creating Lists Using the list() constructor: Lists can also be created
+#using the list() constructor.
+#The list() constructor is used to convert an iterable into a list.
+
+name = 'saurabh'
+lst = list(name)
+
+print(name)
+print(lst)
