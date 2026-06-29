994f70c674488054c6cb0b1bf9e3b0837d50d500
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:35:41 +0530

    add sorted() function
    A built-in function that returns a new sorted list instead of
    #modifying the original list.

---
+++ List/sorted()_function.py
@@ -0,0 +1,8 @@
+#sorted(): A built-in function that returns a new sorted list instead of
+#modifying the original list.
+
+numbers = [19, 2, 35, 1, 67, 41]
+sorted_numbers = sorted(numbers)
+
+print(sorted_numbers) 
+print(numbers) 
