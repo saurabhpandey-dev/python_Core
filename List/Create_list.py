4b2407a78c408b75a210915c0468348d7d262f65
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:23:44 +0530

    add list data type
    list data type

---
+++ List/Create_list.py
@@ -0,0 +1,7 @@
+#The list data type is an ordered sequence of elements that can be composed of
+#strings, numbers or even other lists. Lists are mutable and zero based indexed.
+
+
+city = ['Mirzapur','Varanasi','Bhadohi','Prayagraj']
+
+print(city)
