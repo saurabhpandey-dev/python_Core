dec24d33b95786cb4065b6620f2bda30320bba0e
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:28:19 +0530

    add Checking Element Exists
    Checking if an Element Exists in a List

---
+++ List/Element_Exists_in_List.py
@@ -0,0 +1,7 @@
+#Checking if an Element Exists in a List: The in keyword can be used to
+#check if an element exists in a list.
+
+programming_languages = ['Python', 'Java', 'C++', 'Rust']
+
+print('Rust' in programming_languages) 
+print('JavaScript' in programming_languages )
