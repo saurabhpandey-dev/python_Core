16bc783b81e8c81af67dddfcd1baa4a8ee6446a3
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:25:15 +0530

    add Nesting Lists
    Lists can be nested inside other lists

---
+++ List/Nesting_Lists.py
@@ -0,0 +1,10 @@
+#Nesting Lists: Lists can be nested inside other lists:
+
+developer = ['Alice', 25, ['Python', 'Rust', 'C++']]
+
+#To access the nested list, you will need to access it using index 2 since
+#lists are zero-based indexed.
+
+
+developer = ['Alice', 25, ['Python', 'Rust', 'C++']]
+print(developer[2] )
