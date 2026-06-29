3fe854abd378acb41883d8572f6a155c030bc844
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:31:50 +0530

    add extend() function
    Used to add multiple items to the end of a list.

---
+++ List/extend()_function.py
@@ -0,0 +1,8 @@
+#extend(): Used to add multiple items to the end of a list. Here is an example
+#of adding the numbers 6, 8, and 10 to the end of the numbers list:
+
+numbers = [1, 2, 3, 4, 5]
+even_numbers = [6, 8, 10]
+
+numbers.extend(even_numbers)
+print(numbers)
