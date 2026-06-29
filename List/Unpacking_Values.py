49c7612383786a584c8ba3b387737c80131474a8
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:26:26 +0530

    add Unpacking Values
    Unpacking Values from a List

---
+++ List/Unpacking_Values.py
@@ -0,0 +1,10 @@
+#Unpacking Values from a List: Unpacking values from a list is a technique
+#used to assign values from a list to new variables. Here is an example
+#to unpack the developer list into new variables called name, age and job
+#like this.
+
+developer = ['Alice', 34, 'Rust Developer']
+name, age, job = developer
+print(name)
+print(age)
+print(job)
