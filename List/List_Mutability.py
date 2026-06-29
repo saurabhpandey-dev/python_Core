26de1c0545b2987150e16d205f73fea498fae2f3
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:28:45 +0530

    List Mutability
    Lists are mutable, meaning you can update any element in the
    #list as long as you pass in a valid index number.

---
+++ List/List_Mutability.py
@@ -0,0 +1,8 @@
+#List Mutability: Lists are mutable, meaning you can update any element in the
+#list as long as you pass in a valid index number. To update lists at a
+#particular index, you can assign a new value to that index.
+
+programming_languages = ['Python', 'Java', 'C++', 'Rust']
+print(programming_languages)
+programming_languages[0] = 'JavaScript'
+print(programming_languages)
