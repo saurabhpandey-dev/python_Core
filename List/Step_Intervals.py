c2945b2a8941859b73392d90e9c94060a179b14f
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:35:18 +0530

    add Step Intervals
    Step Intervals

---
+++ List/Step_Intervals.py
@@ -0,0 +1,6 @@
+Step Intervals: It is also possible to specify a step interval which
+determines how much to increment between the indices. Here is an example
+if you want to extract a list of just even numbers using slicing.
+
+numbers = [1, 2, 3, 4, 5, 6]
+print(numbers[1::2] )
