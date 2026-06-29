796c68fe615cb7e6ffb05a97c477c1411d4adb9a
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-29 21:33:31 +0530

    add pop() function
    Used to remove a specific item from the list and return it

---
+++ List/pop()_function.py
@@ -0,0 +1,10 @@
+#pop(): Used to remove a specific item from the list and return it.
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers.pop(1)) # The number 2 is returned
+
+#If you don't specify an element for the pop method, then the last element is
+#removed.
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers.pop())
