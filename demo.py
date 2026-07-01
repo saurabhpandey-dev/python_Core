fe5bb4bab042abd0f567ad592dec4e5c74177920
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-01 23:40:18 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,16 +1,12 @@
-def main():
-    x = get_input()
-    print(f'x is {x}')
+class Student:
 
+    def __init__ (self, name):
+        self.name = name
+    
+    def __str__(self):
+        return self.name
 
-def get_input():
-    while True:
-        try:
-            return int(input('enter the value : '))
-        except ValueError:
-            print('this is not a interger')
-        else:
-            print('the value')
+ob1 = Student('ram')
+ob2 = Student('sita')
+ob3 = Student('rita')
 
-
-main()
