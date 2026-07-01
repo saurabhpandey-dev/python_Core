623abd4c15b50b673f37954dccb0adb6d3905c35
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-01 23:41:39 +0530

    add setattr() function
    lets you create a new attribute or update an existing one dynamically.

---
+++ Handle_Object_Attributes_Dynamically/setattr()_function.py
@@ -0,0 +1,22 @@
+# The setattr() function lets you create a new attribute or update an existing one dynamically.
+
+class Student:
+    def __init__ (self,name,age):
+        self.name=name
+        self.age=age
+    
+    
+
+stu1 = Student('Saurabh',23)
+
+att_name = input('Enter the name : ')
+att_value = input('Enter the value : ')
+
+setattr(stu1,att_name,att_value)
+
+for attr in dir(stu1):
+    if not attr.startswith('__'):
+        value=getattr(stu1,attr)
+        print(f'{attr} : {value}')
+
+
