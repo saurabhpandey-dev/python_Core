a6fec4df04a4b8a0bd7c3185f137d78cbce39fce
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-01 23:42:18 +0530

    add dir() function
    in Python is a built-in tool used to discover and list all valid attributes

---
+++ Handle_Object_Attributes_Dynamically/dir()_function.py
@@ -0,0 +1,10 @@
+#The dir() function in Python is a built-in tool used to discover and
+#list all valid attributes, methods, and properties of any given object.
+import math
+x = 10
+y = "Hello"
+print(dir())
+print()
+print(dir(x)) 
+print()
+print(dir(math))
