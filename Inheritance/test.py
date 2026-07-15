f4fe1d97b085ad458c30ed24d18067b5e695b798
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-15 23:37:15 +0530

    Create test.py

---
+++ Inheritance/test.py
@@ -0,0 +1,23 @@
+class Company:
+    _thought = 'Built for Nation'
+    __name  = 'TATA'
+    def showA(self):
+        print(_thought)
+        print(_name)
+
+class Car(Company):
+    _car = ''
+    _modal = ''
+    __number = 0
+    
+    def number(self):
+        return __number
+    
+    def set_number(self,data):
+        self.__number = data
+        
+car = Car()
+car.
+
+print(car.)
+        
