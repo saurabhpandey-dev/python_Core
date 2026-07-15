e46656880f903dca6030c4630c4e0bf7701b46c9
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-15 14:09:32 +0530

    add multiple inhritance
    multiple inhritance

---
+++ Inheritance/multiple_inheritance.py
@@ -0,0 +1,16 @@
+class A:
+    def name(self):
+        print('Saurabh')
+
+class B:
+    def title(self):
+        print('Pandey')
+
+class C(A,B):
+    def print_all(self):
+        print('My name is Saurabh G. Pandey.')
+        
+obj = C()
+obj.name()
+obj.title()
+obj.print_all()
