fd6f5a071349a1266a5066badfb3f2400ddcd385
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-16 22:49:41 +0530

    add create a file using py
    open('here is file location','here is file modes')

---
+++ File_handling/create_file.py
@@ -0,0 +1,10 @@
+f = open('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\File_handling\\ram.txt','x')
+
+# open('here is file location','here is file modes')
+# 'x' mode is for create empty file 
+
+print('file Created sucessfully!')
+
+# in Python is used to open files on your computer and interact with them by returning a 
+# file object. It serves as the primary tool for file handling operations like reading, 
+# writing, appending, or creating files.
