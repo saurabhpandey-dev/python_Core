b1b2e7481fd1153e7a0614bfc8eef25c1e3c27c3
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-17 23:37:43 +0530

    add readline() function
    it's return only frist line of the file

---
+++ File_handling/readline()_function.py
@@ -0,0 +1,5 @@
+f = open('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\File_handling\\ram.txt','r')
+# here opened the file 'r' mode for read the data from inside the exiting file
+
+print(f.readline()) # it's print only frist line of the file
+f.close()
