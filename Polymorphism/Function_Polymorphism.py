de3457e3e42700a20e6aaec858e62ca645196dd0
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-23 00:07:47 +0530

    add polymorphism
    Derived from Greek, the word literally means "many forms".

---
+++ Polymorphism/Function_Polymorphism.py
@@ -0,0 +1,13 @@
+# In Python is a core Object-Oriented Programming (OOP) concept that allows 
+# a single interface to handle different underlying data types or classes. 
+# Derived from Greek, the word literally means "many forms.". 
+# In practice, it means that different objects can respond to the same
+# method call in their own unique way.
+
+# The built-in len() function seamlessly evaluates the size of various data formats.
+
+print(len('Saurabh'))  #here 7 becouse this is string
+
+print(len(['Saurabh','Ganguly','Pandey'])) # here 3 becouse this is list
+
+print(len({"a": 1, "b": 2})) # here 2 becouse this is dict
