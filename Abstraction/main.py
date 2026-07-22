ebcc06b26a833b2d02c170d80adf924f86169bdc
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-22 21:26:41 +0530

    add abstraction
    learn and reach abstraction

---
+++ Abstraction/main.py
@@ -0,0 +1,11 @@
+import frist as f 
+import second as s
+
+print(f.add(10,5))  #15
+print(s.sub(10,5))  #5
+
+
+# an abstract class is a class that cannot be instantiated on its own and is 
+# designed to be a blueprint for other classes. Abstract classes allow us to 
+# define methods that must be implemented by subclasses, ensuring a consistent 
+# interface while still allowing the subclasses to provide specific implementations.
