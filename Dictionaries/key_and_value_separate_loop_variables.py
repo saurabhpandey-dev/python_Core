59b82a91a4677b566cc472beb0f239345994fe1b
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-22 21:27:16 +0530

    Create key_and_value_separate_loop_variables.py

---
+++ Dictionaries/key_and_value_separate_loop_variables.py
@@ -0,0 +1,23 @@
+products = {
+    'Laptop': 990,
+    'Smartphone': 600,
+    'Tablet': 250,
+    'Headphones': 70,
+}
+
+for product , price in products.items():
+    print(product,price)
+
+
+
+##
+##want to store the key and the value in separate loop variables,
+##you just need to define them and separate them with a comma.
+##Then, you can use them in the body of the loop.
+##
+##Here, we are defining a product loop variable and a price loop variable.
+##Each one will hold its corresponding value. It's important to define
+##them in order – the key first, and then the value.
+##
+##We are printing them side by side, but you can use these values as
+##you need them in your code.
