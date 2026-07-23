f4a6589220b4555e917c68c1a97d949e277a868b
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-23 23:25:08 +0530

    add accessing the element
    accessing the elements of tuple

---
+++ Tuple/accessing_element.py
@@ -0,0 +1,5 @@
+fruits = ("apple", "banana", "cherry", "orange")
+
+print(fruits[1])       # Output: banana (Positive index)
+print(fruits[-1])      # Output: orange (Negative index)
+print(fruits[1:3])     # Output: ('banana', 'cherry') (Slicing)
