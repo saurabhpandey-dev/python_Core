394c1ac38fbb5b5e51c491f85c72e9a48c0c5b50
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-23 23:24:28 +0530

    add tuple
    tuple in Python is a built-in collection data type used to store
    multiple items in a single variable. Its three defining characteristics
    are that it is ordered, immutable (unchangeable), and allows duplicate  values. Tuples are written with round brackets ()

---
+++ Tuple/create_tuple.py
@@ -0,0 +1,18 @@
+# A tuple in Python is a built-in collection data type used to store 
+# multiple items in a single variable. Its three defining characteristics 
+# are that it is ordered, immutable (unchangeable), and allows duplicate 
+# values. Tuples are written with round brackets ().
+
+# Standard tuple
+coordinates = (4, 5, 9)
+
+# Mixed data types
+user_info = ("Alice", 30, True)
+
+# Creating a tuple with ONE item (Crucial: requires a trailing comma!)
+single_item_tuple = ("apple",) 
+
+# Ordered: Elements have a defined sequence that will not change.
+# Immutable: You cannot add, remove, or modify items after creation.
+# Indexed: Elements are accessed using zero-based indexing (e.g., my_tuple[0]).
+# Heterogeneous: A single tuple can hold a mix of different data types.
