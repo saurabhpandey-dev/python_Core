ac2bf379a6c67b98931c24ddccccfa06e78108f5
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-26 23:53:24 +0530

    add Explicit casting

---
+++ Type_conversion/explicit.py
@@ -0,0 +1,9 @@
+# Explicit casting hum khud int(), float(), str(), list() jaise functions se 
+# karte hain.
+
+string = '10'  # this is str 'string'
+
+interger = int(string) # int() function change str to int if str is look like int
+print(interger)
+flooat = float(string) # float() function change str to float if str is look like int
+print(flooat)
