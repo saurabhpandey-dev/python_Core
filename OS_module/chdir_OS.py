735468403d2da2b379e464830e77f3a3a3875aa1
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-30 22:45:54 +0530

    add chdir() function
    it change the working directory

---
+++ OS_module/chdir_OS.py
@@ -0,0 +1,8 @@
+import os
+
+print(os.getcwd())
+
+os.chdir('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\OS_module')
+# chdir(file location with double back-slase)
+# it change the working directory
+print(os.getcwd())
