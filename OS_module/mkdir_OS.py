12eade1c53bf949310a7181bf8ff230b084e8b43
Saurabh Pandey <sp8355063538@gmail.com>
2026-08-01 23:08:12 +0530

    add mkdir() function
    it create the folder in current working directory

---
+++ OS_module/mkdir_OS.py
@@ -0,0 +1,8 @@
+import os
+
+print(os.getcwd())
+os.chdir('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\OS_module')
+print(os.getcwd())
+
+os.mkdir('newfolder') # mkdir(folder name)
+# it create the folder in current working directory
