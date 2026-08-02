007c6c3b358c7f0bdd0cd127bd75d8f88a4dd67f
Saurabh Pandey <sp8355063538@gmail.com>
2026-08-02 23:23:18 +0530

    add rename() function

---
+++ OS_module/rename_OS.py
@@ -0,0 +1,14 @@
+import os 
+
+os.chdir('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\OS_module')
+
+print(os.getcwd())
+
+os.mkdir('ram')
+
+print(os.listdir('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\OS_module'))
+
+print('\n')
+os.rename('ram','sita')
+
+print(os.listdir('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\OS_module'))
