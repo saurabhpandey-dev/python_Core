e05c6cf35c68a7e0c8ae2ea8f6a005e12fffbe2f
Saurabh Pandey <sp8355063538@gmail.com>
2026-08-02 23:21:58 +0530

    add os.path.basename() function
    it return the base name of a file
    path is a sub-module of 'os' module

---
+++ OS_module/path_basename_OS.py
@@ -0,0 +1,9 @@
+import os
+
+os.chdir('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\OS_module')
+
+print(os.getcwd())
+
+print(os.path.basename('os.txt')) # it return the base name of a file
+
+# path is a sub-module of 'os' module
