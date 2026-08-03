09f84f1ff6052801fabf213fe5700e8dea9f0cc4
Saurabh Pandey <sp8355063538@gmail.com>
2026-08-03 23:06:04 +0530

    add os.path.split() function
    it return 2 elements in the form of tuple one for the foldar and one for the file

---
+++ OS_module/path_split_OS.py
@@ -0,0 +1,9 @@
+import os
+
+os.chdir('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\OS_module')
+
+print(os.getcwd())
+
+print(os.path.split('python_Core\\OS_module\\os.txt'))
+
+# it return 2 elements in the form of tuple one for the foldar and one for the file
