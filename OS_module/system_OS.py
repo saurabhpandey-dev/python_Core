475fd1f0d3c50e29063c3287bd9170fc543acd58
Saurabh Pandey <sp8355063538@gmail.com>
2026-08-03 23:04:46 +0530

    add system() function
    if we want to run all cmd or linux terminal command then we use system() function

---
+++ OS_module/system_OS.py
@@ -0,0 +1,7 @@
+import os 
+
+# if we want to run all cmd or linux terminal command then we use system() function
+
+os.system('dir')  # system('cammand') 'dir' for windows and 'ls' fo Linux
+
+# enter the cammand and run the file
