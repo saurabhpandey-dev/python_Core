d5d3af237e7f9b7fbb493ed790d7c7d763836ba4
Saurabh Pandey <sp8355063538@gmail.com>
2026-08-04 22:55:40 +0530

    add os.path.dirname() function
    return directory names from the given path be ensure folder name has been inserted in the parameter

---
+++ OS_module/path_dirname_OS.py
@@ -0,0 +1,8 @@
+import os
+
+os.chdir('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\OS_module')
+
+print(os.getcwd())
+
+print(os.path.dirname('pathon_Core//OS_module//os.txt')) # return directory names from the given path
+ # be ensure folder name has been inserted in the parameter
