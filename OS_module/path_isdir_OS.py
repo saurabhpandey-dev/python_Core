e3888c18b165163202faa5d84103381dccf9086f
Saurabh Pandey <sp8355063538@gmail.com>
2026-08-04 22:57:02 +0530

    add os.path.isdir() function
    this return True becouse of it is a folder

---
+++ OS_module/path_isdir_OS.py
@@ -0,0 +1,10 @@
+import os
+
+os.chdir('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\OS_module')
+
+print(os.path.isdir('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\OS_module'))
+# this return True becouse of it is a folder
+
+print(os.path.isdir('os.txt'))
+
+# return false becouse it is a file 
