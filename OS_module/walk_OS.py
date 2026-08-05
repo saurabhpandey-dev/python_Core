b0ef4e334e0b810388fac75a02c78397a25dac20
Saurabh Pandey <sp8355063538@gmail.com>
2026-08-05 20:52:58 +0530

    Update walk_OS.py

--- OS_module/walk_OS.py
+++ OS_module/walk_OS.py
@@ -18,3 +18,5 @@ for current_dir, list_dir, file_list in vars:
 # <generator object walk at 0x000001C2BD1E18B0>
 # ('C:\\Users\\Saurabh Pandey\\Desktop\\python_Core\\OS_module', [], ['chdir_OS.py', 'cwd_OS.py', 'listdir_OS.py', 'makedirs_OS.py', 'mk
 # dir_OS.py', 'os.txt', 'removedirs_OS.py', 'rmdir_OS.py', 'use_OS.py', 'walk_OS.py'])
+
+# (folder ka pta , [] , useke andar ki sari file ki list)
